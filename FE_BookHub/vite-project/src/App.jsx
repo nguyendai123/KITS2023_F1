5245a161ea79d6796b226a76614a7b9fae375c64
nguyendai123 <nguyenvandai1232001@gmail.com>
2023-08-20 07:01:42 +0700

    create home

--- FE_BookHub/vite-project/src/App.jsx
+++ FE_BookHub/vite-project/src/App.jsx
@@ -3,13 +3,19 @@ import Login from "./components/Login/Login";
 import Home from "./components/Home/Home";
 
 import "./App.css";
+import Register from "./components/Register/Register";
+import BookItemDetails from "./components/BookItemDetails/BookItemDetails";
+import BookPage from "./components/BookPage/BookPage";
 
 const App = () => {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="login" element={<Login />} />
+        <Route path="login" exact element={<Login />} />
+        <Route path="register" exact element={<Register />} />
+        <Route path="/books/:id" exact element={<BookItemDetails />} />
+        <Route path="/books" exact element={<BookPage />} />
       </Routes>
     </div>
   );
--- FE_BookHub/vite-project/src/components/Header/Header.jsx
+++ FE_BookHub/vite-project/src/components/Header/Header.jsx
@@ -1,76 +1,112 @@
-import {Link, redirect} from 'react-router-dom'
-import Cookies from 'js-cookie'
+import { Link, redirect } from "react-router-dom";
+import Cookies from "js-cookie";
+import { useNavigate } from "react-router-dom";
+import { FiMenu } from "react-icons/fi";
+import { RiCloseCircleFill } from "react-icons/ri";
 
-import {FiMenu} from 'react-icons/fi'
-import {RiCloseCircleFill} from 'react-icons/ri'
-import './Header.css'
-import { useState } from 'react'
+import "./Header.css";
+import { useState } from "react";
 
 // eslint-disable-next-line react/prop-types
-const Header =({page})=> {
-  const[displayNavbar,setDisplayNavbar] = useState(false)
-
+const Header = ({ page }) => {
+  const [displayNavbar, setDisplayNavbar] = useState(false);
+  let navigate = useNavigate();
   const onClickMenu = () => {
-    
-      setDisplayNavbar(displayNavbar)
-    
-  }
+    setDisplayNavbar(!displayNavbar);
+  };
 
- const onClickCross = () => {
-    setDisplayNavbar(false)
-  }
+  const onClickCross = () => {
+    setDisplayNavbar(false);
+  };
 
   const onClickLogout = () => {
-    console.log('logout')
-    Cookies.remove('jwt_token')
-   return redirect('/login')
-    
-  }
+    console.log("logout");
+    Cookies.remove("jwt_token");
+    return navigate("/login");
+  };
 
   const onClickWebSiteLogo = () => {
-   return redirect('/login')
-  }
+    return navigate("/");
+  };
 
-  
-    // eslint-disable-next-line react/prop-types
-    const {home, shelves, favorite} = page;
-    const activeHome = home ? 'active-tab' : ''
-    const activeShelves = shelves ? 'active-tab' : ''
-    const activeFavorite = favorite ? 'active-tab' : ''
-    
+  // eslint-disable-next-line react/prop-types
+  const { home, shelves, favorite } = page;
+  const activeHome = home ? "active-tab" : "";
+  const activeShelves = shelves ? "active-tab" : "";
+  const activeFavorite = favorite ? "active-tab" : "";
 
-    return (
-      <div>
-        <div className="header-container">
-          <div className="header-website-logo1">
-            <Link to="/">
-              <>
-                <img
-                  className="header-website-logo"
-                  src="https://res.cloudinary.com/dkxxgpzd8/image/upload/v1647190320/Group_7731_v0p1nt_gjeokw.png"
-                  alt="website logo"
-                  onClick={onClickWebSiteLogo}
-                />
-              </>
-            </Link>
-          </div>
-          <ul className="tabs-container">
-            <Link className="link" to="/">
-              <li className={`list-item bookshelves-tab ${activeHome}`}>
-                Home
-              </li>
-            </Link>
-            <Link className="link" to="/shelf">
-              <li className={`list-item bookshelves-tab ${activeShelves}`}>
-                Bookshelves
-              </li>
-            </Link>
-            <Link className="link" to="/favorites">
-              <li className={`list-item bookshelves-tab ${activeFavorite}`}>
-                MyFavorites
-              </li>
-            </Link>
-            <li className="list-item">
+  return (
+    <div>
+      <div className="header-container">
+        <div className="header-website-logo1">
+          <Link to="/">
+            <>
+              <img
+                className="header-website-logo"
+                src="https://res.cloudinary.com/dkxxgpzd8/image/upload/v1647190320/Group_7731_v0p1nt_gjeokw.png"
+                alt="website logo"
+                onClick={onClickWebSiteLogo}
+              />
+            </>
+          </Link>
+        </div>
+        <ul className="tabs-container">
+          <Link className="link" to="/">
+            <li className={`list-item  ${activeHome}`}>Home</li>
+          </Link>
+          <Link className="link" to="/shelf">
+            <li className={`list-item ${activeShelves}`}>Bookshelves</li>
+          </Link>
+          <Link className="link" to="/favorites">
+            <li className={`list-item ${activeFavorite}`}>MyFavorites</li>
+          </Link>
+          <li className="list-item">
+            <button
+              onClick={onClickLogout}
+              className="logout-btn"
+              type="button"
+            >
+              Logout
+            </button>
+          </li>
+        </ul>
+      </div>
+      <div className="header-navbar-responsive-container">
+        <div className="header-nav-container">
+          <Link to="/">
+            <img
+              className="header-nav-bar-website-logo"
+              src="https://res.cloudinary.com/dkxxgpzd8/image/upload/v1647190320/Group_7731_v0p1nt_gjeokw.png"
+              alt="website logo"
+              onClick={onClickWebSiteLogo}
+            />
+          </Link>
+          <button
+            onClick={onClickMenu}
+            className="cross-icon-btn"
+            type="button"
+          >
+            <FiMenu className="menu-icon" />
+          </button>
+        </div>
+        {displayNavbar && (
+          <>
+            <div className="header-navbar-tabs-container">
+              <Link className="link" to="/">
+                <h1 className={`list-item home-tab ${activeHome}`}>Home</h1>
+              </Link>
+              <Link className="link" to="/shelf">
+                <h1 className={`bookshelves-tab ${activeShelves}`}>
+                  BookShelves
+                </h1>
+              </Link>
+              <Link className="link" to="/favorites">
+                <h1 className={`bookshelves-tab ${activeFavorite}`}>
+                  MyFavorites
+                </h1>
+              </Link>
+            </div>
+            <div className="header-navbar-tabs-container">
               <button
                 onClick={onClickLogout}
                 className="logout-btn"
@@ -78,66 +114,19 @@ const Header =({page})=> {
               >
                 Logout
               </button>
-            </li>
-          </ul>
-        </div>
-        <div className="header-navbar-responsive-container">
-          <div className="header-nav-container">
-            <Link to="/">
-              <img
-                className="header-nav-bar-website-logo"
-                src="https://res.cloudinary.com/dkxxgpzd8/image/upload/v1647190320/Group_7731_v0p1nt_gjeokw.png"
-                alt="website logo"
-                onClick={onClickWebSiteLogo}
-              />
-            </Link>
-            <button
-              onClick={onClickMenu}
-              className="cross-icon-btn"
-              type="button"
-            >
-              <FiMenu className="menu-icon" />
-            </button>
-          </div>
-          {displayNavbar && (
-            <>
-              <div className="header-navbar-tabs-container">
-                <Link className="link" to="/">
-                  <h1 className={`home-tab ${activeHome}`}>Home</h1>
-                </Link>
-                <Link className="link" to="/shelf">
-                  <h1 className={`bookshelves-tab ${activeShelves}`}>
-                    BookShelves
-                  </h1>
-                </Link>
-                <Link className="link" to="/favorites">
-                  <h1 className={`bookshelves-tab ${activeFavorite}`}>
-                    MyFavorites
-                  </h1>
-                </Link>
-              </div>
-              <div className="header-navbar-tabs-container">
-                <button
-                  onClick={onClickLogout}
-                  className="logout-btn"
-                  type="button"
-                >
-                  Logout
-                </button>
-                <button
-                  onClick={onClickCross}
-                  className="cross-icon-btn"
-                  type="button"
-                >
-                  <RiCloseCircleFill className="cross-icon" />
-                </button>
-              </div>
-            </>
-          )}
-        </div>
+              <button
+                onClick={onClickCross}
+                className="cross-icon-btn"
+                type="button"
+              >
+                <RiCloseCircleFill className="cross-icon" />
+              </button>
+            </div>
+          </>
+        )}
       </div>
-    )
-  
-}
+    </div>
+  );
+};
 
-export default Header
+export default Header;
